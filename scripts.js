c53ea56f557d8f566fb72696307a79307dbf5709
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-07-20 23:21:24 -0500

    Terminado!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 //Variables
-let presupuesto = prompt("Ingresa tu presupuesto total")
-let form = document.querySelector('form')
-let registroCarro = {
-    name: "Transporte",
-    monto: 200
-}
+
+    //Generales
+let form = document.querySelector('form') //usado en la clase Interface
+let btn = document.querySelector('button')
+
+
+
+    //Leen data
+let presupuesto = Number(prompt("Ingresa tu presupuesto total, es NECESARIO."))
+let restante = presupuesto
+let gastoNombre = document.querySelector('#gastoNombre')
+let gastoMonto = document.querySelector('#gastoMonto')
+
+
 
 //Clases
 
@@ -15,7 +23,7 @@ class Interface{
         
         presupuestoP.classList.add('presupuesto-text')
         presupuestoP.innerHTML = `
-            Tu presupuesto ingresado es de: <b>${presupuesto}</b>
+            Tu presupuesto: <b>${presupuesto}</b>
         `;
 
         presupuestoBox.append(presupuestoP)
@@ -24,12 +32,20 @@ class Interface{
 
     }
 
-    printRestante(restante){
+    printRestante(restante,alerta="normalPresupuesto"){
         let restanteBox = document.querySelector('.box_out_data_restante')
+        let restanteBox1P = document.querySelector('.box_out_data_restante p')
+
+        restanteBox.classList.add(alerta)
+
+        if (restanteBox1P) {
+            restanteBox1P.remove()
+        }
+
         let restanteP = document.createElement("p");
 
         restanteP.innerHTML = `
-        Tu presupuesto restante es de: <b>${restante}</b>
+        Despues de gastos: <b>${restante}</b>
     `;
 
         restanteBox.append(restanteP)
@@ -41,6 +57,10 @@ class Interface{
         mensajeP.classList.add("error")
 
         form.appendChild(mensajeP)
+
+        setTimeout(() => {
+            mensajeP.remove()
+        }, 2000);
     }
 
     printNuevoRegistro(nuevoRegistro){
@@ -60,12 +80,97 @@ class Interface{
     }
 }
 
+
 let ui = new Interface()
 
+
 ui.printPresupuesto(presupuesto)
-ui.printRestante(presupuesto)
-ui.printError("Faltan datos")
-ui.printNuevoRegistro(registroCarro)
+ui.printRestante(restante)
+//ui.printError("Faltan datos")
 
 
 //EventListeners
+
+btn.addEventListener("click",validarRegistrar)
+
+
+document.addEventListener("DOMContentLoaded",validarIngreso)
+
+
+
+
+//Funciones
+function validarRegistrar(e) {
+    e.preventDefault()
+
+    //Validar que haya datos
+    let validarDatos = validar()
+
+    if (validarDatos) {
+        //console.log("Habemus data");
+
+        //Registrar Data
+        registrar()
+
+        //Descontar dinero
+        descontar()
+
+    } else{
+        //console.log("NO habemus data");
+
+    }
+    
+}
+
+function validar() {
+    //console.log("Hola desde validar");
+    //console.log(gastoNombre.value);
+    //console.log(gastoMonto.value);
+
+    if (gastoNombre.value && gastoMonto.value) {
+        //console.log("Estan llenos");
+        return true
+
+    } else {
+        //console.log("Estan vacios");
+        ui.printError("Faltan datos")
+        return false
+    }
+}
+
+function validarIngreso(){
+    if (!presupuesto) {
+        location.reload();
+    }
+}
+
+function registrar(){
+    
+    let nuevoRegistro = {
+        name: gastoNombre.value,
+        monto: Number(gastoMonto.value)
+    }
+
+    //console.log(nuevoRegistro.name);
+    //console.log(nuevoRegistro.monto);
+
+    ui.printNuevoRegistro(nuevoRegistro)
+
+}
+
+function descontar(){
+    let alerta;
+    restante = restante - Number(gastoMonto.value)
+
+    if (presupuesto/5 >= restante) {
+        alerta = "peligroPresupuesto"
+        console.log("peligro");
+
+    } else if(presupuesto/2 >= restante) {
+        alerta = "cuidadoPresupuesto"
+        console.log("cuidado");
+    }
+
+    ui.printRestante(restante,alerta)
+
+}
